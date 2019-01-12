b9a98b93ec05650227706ead065c0a1dc372f4d0
Kazeke <kazbek_kaddirov@mail.ru>
2019-01-12 22:33:57 +0500

    Урок 3 обновление

--- src/ru/geekbrains/guess_the_number/Main.java
+++ src/ru/geekbrains/guess_the_number/Main.java
@@ -3,12 +3,19 @@ package ru.geekbrains.guess_the_number;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println(" Ваша задача угадать число.");
-        int range = 20;
+        int range = 30;
         int number = (int) (Math.random() * range);
+        playLevel(range, number);
+
+        scanner.close();
+    }
+
+    private static void playLevel(int range, int number) {
         while (true) {
             System.out.println(" Угадайте число от 0 до " + range);
             int input_number = scanner.nextInt();
@@ -22,6 +29,5 @@ public class Main {
             }
 
         }
-        scanner.close();
     }
 }
